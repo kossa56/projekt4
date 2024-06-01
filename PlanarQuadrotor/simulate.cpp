7f2afd3c60d0f53ce3741095c4caa118a40496b0
_kossa_ <101972455+kossa56@users.noreply.github.com>
2024-06-01 18:05:52 +0200

    dźwięk silników, SDL

--- PlanarQuadrotor/simulate.cpp
+++ PlanarQuadrotor/simulate.cpp
@@ -2,6 +2,29 @@
  * SDL window creation adapted from https://github.com/isJuhn/DoublePendulum
 */
 #include "simulate.h"
+#include <thread>
+#include <matplot/matplot.h>
+#include <SDL/include/SDL.h>
+
+//dźwięk silników
+void generateEngineSound(Uint8* buffer, int length, double frequency, double left_gain) {
+    const int amplitude = 1; // Amplituda dźwięku
+    const double sampleRate = 44100.0; // Częstotliwość próbkowania
+
+    for (int i = 0; i < length; ++i) {
+        double time = i / sampleRate;
+        double value = amplitude * sin(2.0 * M_PI * frequency * time);
+
+        // Zmiana amplitudy w zależności od kierunku ruchu
+        if (left_gain > 1.0)
+            buffer[i] = (Uint8)(value * left_gain + 2); // Lewy głośnik głośniejszy
+        else if (left_gain < 1.0)
+            buffer[i] = (Uint8)(value + 2* left_gain); // Prawy głośnik głośniejszy
+        else
+            buffer[i] = (Uint8)(value + 2); // Równa amplituda dla obu głośników
+    }
+}
+
 
 Eigen::MatrixXf LQR(PlanarQuadrotor &quadrotor, float dt) {
     //LQR
@@ -33,10 +56,35 @@ void control(PlanarQuadrotor &quadrotor, const Eigen::MatrixXf &K) {
 
 int main(int argc, char* args[])
 {
+     // Inicjalizacja SDL
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0) {
+        std::cerr << "SDL initialization failed: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    // Konfiguracja specyfikacji audio
+    SDL_AudioSpec spec;
+    spec.freq = 54100; // Częstotliwość próbkowania
+    spec.format = AUDIO_U8; // Format dźwięku
+    spec.channels = 2; // Liczba kanałów (stereo)
+    spec.samples = 8096; // Rozmiar bufora próbek
+    spec.callback = NULL; // Brak funkcji callback
+    spec.userdata = NULL;
+
+    // Otwarcie urządzenia audio
+    SDL_AudioDeviceID deviceId = SDL_OpenAudioDevice(NULL, 0, &spec, NULL, 0);
+    if (deviceId == 0) {
+        std::cerr << "Failed to open audio device: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return -1;
+    }
+
     std::shared_ptr<SDL_Window> gWindow = nullptr;
     std::shared_ptr<SDL_Renderer> gRenderer = nullptr;
     const int SCREEN_WIDTH = 1280;
     const int SCREEN_HEIGHT = 720;
+    const int start_x=640;
+    const int start_y=360;
 
     /**
      * TODO: Extend simulation
@@ -45,6 +93,7 @@ int main(int argc, char* args[])
      * 2. Update PlanarQuadrotor from simulation when goal is changed
     */
     Eigen::VectorXf initial_state = Eigen::VectorXf::Zero(6);
+    initial_state << start_x, start_y, 0, 0, 0, 0; 
     PlanarQuadrotor quadrotor(initial_state);
     PlanarQuadrotorVisualizer quadrotor_visualizer(&quadrotor);
     /**
@@ -56,7 +105,7 @@ int main(int argc, char* args[])
     goal_state << -1, 7, 0, 0, 0, 0;
     quadrotor.SetGoal(goal_state);
     /* Timestep for the simulation */
-    const float dt = 0.05;
+    const float dt = 0.01;
     Eigen::MatrixXf K = LQR(quadrotor, dt);
     Eigen::Vector2f input = Eigen::Vector2f::Zero(2);
 
@@ -115,9 +164,22 @@ int main(int argc, char* args[])
 
             /* Simulate quadrotor forward in time */
             control(quadrotor, K);
-            quadrotor.Update(dt);
-        }
+        quadrotor.Update(dt);
+
+        // Generowanie dźwięku silnika na podstawie ruchu quadrotora
+        double state_theta = quadrotor.GetState()[2]; // Pobierz kąt theta
+        double left_gain = 1 + std::abs(state_theta) / M_PI; // Oblicz współczynnik wzmocnienia dla lewego głośnika
+        double frequency = 150; // Bazowa częstotliwość dźwięku
+        Uint8* audioBuffer = new Uint8[spec.samples];
+        generateEngineSound(audioBuffer, spec.samples, frequency, left_gain);
+        SDL_QueueAudio(deviceId, audioBuffer, spec.samples);
+        SDL_PauseAudioDevice(deviceId, 0);
+        delete[] audioBuffer;
     }
+    
+    }
+    // Zwalnianie zasobów SDL
+    SDL_CloseAudioDevice(deviceId);
     SDL_Quit();
     return 0;
 }
